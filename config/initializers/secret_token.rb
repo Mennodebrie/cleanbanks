821cb48859223be0a6be2f3ed54b6779b05f183c
Menno de Brie <menno@paperchip.nl>
2012-01-11 16:42:29 +0100

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cleanbanks::Application.config.secret_token = 'ca0fb08d27af92c3f721fa2062163ce7966d99ad1fa62fbbeef565d99e88cbddbe63d8e13948cc0cd0a46efc30abee310caae8754214f0b6db2ab6fa0072bbd5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Cleanbanks::Application.config.session_store :cookie_store, :key => '_cleanbanks_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Cleanbanks::Application.config.session_store :active_record_store
