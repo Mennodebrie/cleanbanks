788cd3ff551c7b67bbaa7e8f4f10e1a0457e48ce
Menno de Brie <menno@paperchip.nl>
2013-01-23 12:57:30 +0100

    deployment config files

---
+++ config/unicorn.rb
@@ -0,0 +1,10 @@
+root = "/home/deployer/apps/cleanbanks/current"
+working_directory root
+pid "#{root}/tmp/pids/unicorn.pid"
+stderr_path "#{root}/log/unicorn.log"
+stdout_path "#{root}/log/unicorn.log"
+
+listen "/tmp/unicorn.cleanbanks.sock"
+listen 8081, :tcp_nopush => true
+worker_processes 2
+timeout 30
